cacd1bcaf15d6efdeb0e9ded533fc45793247bbc
Ariel Ebersberger <31776703+justanotherariel@users.noreply.github.com>
2026-08-26 21:13:42 +0200

    Second probe commit (synchronize test)

--- homeassistant/components/aqualogic/switch.py
+++ homeassistant/components/aqualogic/switch.py
@@ -106,3 +106,4 @@ class AquaLogicSwitch(SwitchEntity):
             async_dispatcher_connect(self.hass, UPDATE_TOPIC, self.async_write_ha_state)
         )
 # bot-test: undefined-state probe (bot-test-undef-sync)
+# second probe commit
